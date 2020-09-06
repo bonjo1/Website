eb28598f2e7cb039ce9d92050d33f193bf526135
bonjo1 <70815595+bonjo1@users.noreply.github.com>
2020-09-06 10:30:04 +0800

    Add files via upload

---
+++ website/index.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title> SicMu </title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+		<script src="https://kit.fontawesome.com/f4a395be37.js" crossorigin="anonymous"></script>
+	</head>
+    <body>	
+		<header>
+		<div class="main">
+			<div class="logo">
+                <ul>
+                    <li><a href="#">Menu</a>
+                    </li>
+                </ul>
+                <img src="logo.png">
+            <div class="container">
+                <div class="c-video">
+                    <video class="video" src="videoplayback.mp4" controls></video>>
+            </div>
+		</div>
+		</div>
+		<div class="search-box">
+			<input class="search-txt" type="text" name="" placeholder="Type to search">
+			<class class="search-btn" href="#"></class>
+            <i class="fas fa-search"></i>
+		</div>
+		</div>
+		</header>
+	</body>
+</html>
